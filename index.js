e1e7f4717430c17c613f3f9d847a61fc55db1a3d
phamdinhthanhcntt6 <phamdinhthanhcntt6@gmail.com>
2024-02-20 11:24:26 +0700

    server1

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const authRouter = require("./routers/authRouter");
+
 app.use(cors());
+app.use(express.json());
+
 const PORT = 3001;
-app.get("/auth/hello", (_req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
+
+app.use("/auth", authRouter);
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
---
+++ routers/authRouter.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const authRouter = Router();
+
+authRouter.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("aaa");
+});
+module.exports = authRouter;
