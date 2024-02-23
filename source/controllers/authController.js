10a900e91d1323a30b1a91a192935ace24b5328e
phamdinhthanhcntt6 <phamdinhthanhcntt6@gmail.com>
2024-02-23 15:28:34 +0700

    login backend

--- source/controllers/authController.js
+++ source/controllers/authController.js
@@ -37,7 +37,8 @@ const register = asyncHandle(async (req, res) => {
   res.status(200).json({
     message: "Register new user successfully",
     data: {
-      ...newUser,
+      email: newUser.email,
+      id: newUser.id,
       accesstoken: await getJsonWebToken(email, newUser.id)
     }
   });
@@ -45,6 +46,31 @@ const register = asyncHandle(async (req, res) => {
   //res.send("register");
 });
 
+const login = asyncHandle(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await UserModel.findOne({ email });
+  if (!existingUser) {
+    res.status(403).json({
+      message: "User not found"
+    });
+    throw new Error(`User not found`);
+  }
+  const isMatchPassword = await bcrypt.compare(password, existingUser.password);
+  if (!isMatchPassword) {
+    res.status(401);
+    throw new Error(`Invalid email or password`);
+  }
+  res.status(200).json({
+    message: "Login successfully",
+    data: {
+      email: existingUser.email,
+      id: existingUser.id,
+      accesstoken: await getJsonWebToken(email, existingUser.id)
+    }
+  });
+});
+
 module.exports = {
-  register
+  register,
+  login
 };
